ec02e6f363e40119ca5df1762d9ec2274f3cb4af
shengai <shengai@gmail.com>
2022-06-16 13:59:33 +0800

    添加项目文件。

---
+++ src/Stylet.Avalonia.Sample/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Stylet.Avalonia.Sample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
